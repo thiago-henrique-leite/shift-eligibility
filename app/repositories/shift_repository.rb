5e666fa8184dd48b1a3a1ac26a650deae0bdaf5d
Thiago Henrique Leite da Silva <thiago.leite@redealumni.com>
2024-09-02 20:48:37 -0300

    refactor(repository): optimize worker availability queries

--- app/repositories/shift_repository.rb
+++ app/repositories/shift_repository.rb
@@ -7,32 +7,16 @@ class ShiftRepository
       .for_facilities(facility_ids)
       .for_date_range(start_date, end_date)
       .for_profession(worker.profession)
-      .where.not(
-        'EXISTS (
-          SELECT 1
-          FROM "Shift" AS shifts
-          WHERE shifts.worker_id = ?
-          AND shifts.start < ?
-          AND shifts.ends_at > ?
-        )', worker.id, end_date, start_date
-      )
+      .for_available_worker(worker, start_date, end_date)
+      .distinct
   end
 
   def self.available_facility_ids(worker)
     Facility
       .active
       .joins(:facility_requirements)
-      .where.not(
-        'EXISTS (
-          SELECT 1
-          FROM "FacilityRequirement" AS fr
-          WHERE fr.facility_id = "Facility".id
-          AND fr.document_id = ALL (
-            SELECT DISTINCT d.id
-            FROM "Document" AS d
-            WHERE d.id IN (?)
-          )
-        )', worker.documents.active.distinct.pluck(:id)
-      ).distinct.pluck(:id)
+      .for_available_worker(worker)
+      .distinct
+      .pluck(:id)
   end
 end
