4de04e37183765eabc7eaefd231cf4c38fa592fa
Thiago Henrique Leite da Silva <thiago.leite@redealumni.com>
2024-09-02 20:33:54 -0300

    chore: update database schema and foreign key references in migration files

--- db/schema.rb
+++ db/schema.rb
@@ -10,63 +10,67 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_02_231829) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_01_223856) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  # Custom types defined in this database.
-  # Note that some types may not work with other database engines. Be careful if changing database.
-  create_enum "Profession", ["CNA", "LVN", "RN"]
-
-  create_table "Document", id: :serial, force: :cascade do |t|
+  create_table "Document", force: :cascade do |t|
     t.text "name", null: false
     t.boolean "is_active", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "DocumentWorker", id: :serial, force: :cascade do |t|
-    t.integer "worker_id", null: false
-    t.integer "document_id", null: false
+  create_table "DocumentWorker", force: :cascade do |t|
+    t.bigint "worker_id", null: false
+    t.bigint "document_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["document_id"], name: "index_DocumentWorker_on_document_id"
+    t.index ["worker_id"], name: "index_DocumentWorker_on_worker_id"
   end
 
-  create_table "Facility", id: :serial, force: :cascade do |t|
+  create_table "Facility", force: :cascade do |t|
     t.text "name", null: false
     t.boolean "is_active", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "FacilityRequirement", id: :serial, force: :cascade do |t|
-    t.integer "facility_id", null: false
-    t.integer "document_id", null: false
+  create_table "FacilityRequirement", force: :cascade do |t|
+    t.bigint "facility_id", null: false
+    t.bigint "document_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["document_id"], name: "index_FacilityRequirement_on_document_id"
+    t.index ["facility_id"], name: "index_FacilityRequirement_on_facility_id"
   end
 
-  create_table "Shift", id: :serial, force: :cascade do |t|
+  create_table "Shift", force: :cascade do |t|
     t.datetime "start", precision: 3, null: false
     t.datetime "ends_at", precision: 3, null: false
-    t.enum "profession", null: false, enum_type: ""Profession""
+    t.string "profession", null: false
     t.boolean "is_deleted", default: false, null: false
-    t.integer "facility_id", null: false
-    t.integer "worker_id"
+    t.bigint "facility_id", null: false
+    t.bigint "worker_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["facility_id"], name: "index_Shift_on_facility_id"
+    t.index ["worker_id"], name: "index_Shift_on_worker_id"
   end
 
-  create_table "Worker", id: :serial, force: :cascade do |t|
+  create_table "Worker", force: :cascade do |t|
     t.text "name", null: false
     t.boolean "is_active", default: false, null: false
-    t.enum "profession", null: false, enum_type: ""Profession""
+    t.string "profession", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "_prisma_migrations", id: { type: :string, limit: 36 }, force: :cascade do |t|
-    t.string "checksum", limit: 64, null: false
-    t.timestamptz "finished_at"
-    t.string "migration_name", limit: 255, null: false
-    t.text "logs"
-    t.timestamptz "rolled_back_at"
-    t.timestamptz "started_at", default: -> { "now()" }, null: false
-    t.integer "applied_steps_count", default: 0, null: false
-  end
-
-  add_foreign_key "DocumentWorker", "Document", column: "document_id", name: "DocumentWorker_document_id_fkey", on_update: :cascade, on_delete: :restrict
-  add_foreign_key "DocumentWorker", "Worker", column: "worker_id", name: "DocumentWorker_worker_id_fkey", on_update: :cascade, on_delete: :restrict
-  add_foreign_key "FacilityRequirement", "Document", column: "document_id", name: "FacilityRequirement_document_id_fkey", on_update: :cascade, on_delete: :restrict
-  add_foreign_key "FacilityRequirement", "Facility", column: "facility_id", name: "FacilityRequirement_facility_id_fkey", on_update: :cascade, on_delete: :restrict
-  add_foreign_key "Shift", "Facility", column: "facility_id", name: "Shift_facility_id_fkey", on_update: :cascade, on_delete: :restrict
-  add_foreign_key "Shift", "Worker", column: "worker_id", name: "Shift_worker_id_fkey", on_update: :cascade, on_delete: :nullify
+  add_foreign_key "DocumentWorker", "Document", column: "document_id", on_update: :cascade, on_delete: :restrict
+  add_foreign_key "DocumentWorker", "Worker", column: "worker_id", on_update: :cascade, on_delete: :restrict
+  add_foreign_key "FacilityRequirement", "Document", column: "document_id", on_update: :cascade, on_delete: :restrict
+  add_foreign_key "FacilityRequirement", "Facility", column: "facility_id", on_update: :cascade, on_delete: :restrict
+  add_foreign_key "Shift", "Facility", column: "facility_id", on_update: :cascade, on_delete: :restrict
+  add_foreign_key "Shift", "Worker", column: "worker_id", on_update: :cascade, on_delete: :nullify
 end
