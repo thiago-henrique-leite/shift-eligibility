9614bb3fa8d4c1c640df558b4460b2c3b3f503bf
Thiago Henrique Leite da Silva <thiago.leite@redealumni.com>
2024-09-02 20:47:34 -0300

    refactor(models/facility): add scope for available worker

--- app/models/facility.rb
+++ app/models/facility.rb
@@ -8,4 +8,21 @@ class Facility < ApplicationRecord
   validates :name, presence: true
 
   scope :active, -> { where(is_active: true) }
+
+  scope :for_available_worker, ->(worker) {
+    document_ids = worker.documents.active.distinct.pluck(:id)
+
+    .where.not(
+      'EXISTS (
+        SELECT 1
+        FROM "FacilityRequirement" AS fr
+        WHERE fr.facility_id = "Facility".id
+        AND fr.document_id = ALL (
+          SELECT DISTINCT d.id
+          FROM "Document" AS d
+          WHERE d.id IN (?)
+        )
+      )', document_ids
+    )
+  }
 end
