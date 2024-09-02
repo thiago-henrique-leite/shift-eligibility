b13095a02ec6a072073a26d0293fa87845c1dbba
Thiago Henrique Leite da Silva <thiago.leite@redealumni.com>
2024-09-02 20:32:42 -0300

    test(models/shift): update validation for end column to ends_at

--- spec/models/shift_spec.rb
+++ spec/models/shift_spec.rb
@@ -1,6 +1,6 @@
 RSpec.describe Shift do
   describe 'validations' do
-    it { is_expected.to validate_presence_of(:end) }
+    it { is_expected.to validate_presence_of(:ends_at) }
     it { is_expected.to validate_presence_of(:profession) }
     it { is_expected.to validate_presence_of(:start) }
   end
