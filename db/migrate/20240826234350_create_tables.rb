50bc32719db641d1aa8478f7bbd4e268787171ca
Thiago Henrique Leite da Silva <thiago.leite@redealumni.com>
2024-09-02 20:33:43 -0300

    refactor(models): update foreign key references in migration files

--- db/migrate/20240826234350_create_tables.rb
+++ db/migrate/20240826234350_create_tables.rb
@@ -25,15 +25,15 @@ class CreateTables < ActiveRecord::Migration[7.1]
     end
 
     create_table 'FacilityRequirement' do |t|
-      t.references :facility, null: false, foreign_key: { to_table: :facilities, on_delete: :restrict, on_update: :cascade }
-      t.references :document, null: false, foreign_key: { to_table: :documents, on_delete: :restrict, on_update: :cascade }
+      t.references :facility, null: false, foreign_key: { to_table: :Facility, on_delete: :restrict, on_update: :cascade }
+      t.references :document, null: false, foreign_key: { to_table: :Document, on_delete: :restrict, on_update: :cascade }
 
       t.timestamps
     end
 
     create_table 'DocumentWorker' do |t|
-      t.references :worker, null: false, foreign_key: { to_table: :workers, on_delete: :restrict, on_update: :cascade }
-      t.references :document, null: false, foreign_key: { to_table: :documents, on_delete: :restrict, on_update: :cascade }
+      t.references :worker, null: false, foreign_key: { to_table: :Worker, on_delete: :restrict, on_update: :cascade }
+      t.references :document, null: false, foreign_key: { to_table: :Document, on_delete: :restrict, on_update: :cascade }
 
       t.timestamps
     end
@@ -43,20 +43,20 @@ class CreateTables < ActiveRecord::Migration[7.1]
       t.datetime :end, null: false, precision: 3
       t.string :profession, null: false
       t.boolean :is_deleted, null: false, default: false
-      t.references :facility, null: false, foreign_key: { to_table: :facilities, on_delete: :restrict, on_update: :cascade }
-      t.references :worker, foreign_key: { to_table: :workers, on_delete: :nullify, on_update: :cascade }
+      t.references :facility, null: false, foreign_key: { to_table: :Facility, on_delete: :restrict, on_update: :cascade }
+      t.references :worker, foreign_key: { to_table: :Worker, on_delete: :nullify, on_update: :cascade }
 
       t.timestamps
     end
   end
 
   def down
-    drop_table :shifts
-    drop_table :document_workers
-    drop_table :facility_requirements
-    drop_table :documents
-    drop_table :facilities
-    drop_table :workers
+    drop_table 'Shift'
+    drop_table 'DocumentWorker'
+    drop_table 'FacilityRequirement'
+    drop_table 'Document'
+    drop_table 'Facility'
+    drop_table 'Worker'
     execute <<-SQL
       DROP TYPE "Profession";
     SQL
