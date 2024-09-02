c8851589bca54c1f8b6560ea4c09d4e1696b8a33
Thiago Henrique Leite da Silva <thiago.leite@redealumni.com>
2024-09-02 20:17:39 -0300

    feat(repositories/shift): add ShiftRepository class

---
+++ app/repositories/shift_repository.rb
@@ -0,0 +1,40 @@
+class ShiftRepository
+  def self.available_shifts_for_facility(worker, facility_ids, start_date, end_date)
+    Shift.joins(:facility)
+         .active
+         .unclaimed
+         .for_facilities(facility_ids)
+         .for_date_range(start_date, end_date)
+         .for_profession(worker.profession)
+         .where.not(
+          'EXISTS (
+             SELECT 1
+             FROM "Shift" AS shifts
+             WHERE shifts.worker_id = ?
+             AND shifts.start < ?
+             AND shifts.ends_at > ?
+           )',
+          worker.id, end_date, start_date
+        )
+  end
+
+  def self.available_facility_ids(worker)
+    Facility.active
+      .joins(:facility_requirements)
+      .where.not(
+        'EXISTS (
+          SELECT 1
+          FROM "FacilityRequirement" AS fr
+          WHERE fr.facility_id = "Facility".id
+          AND fr.document_id = ALL (
+            SELECT DISTINCT d.id
+            FROM "Document" AS d
+            WHERE d.id IN (?)
+          )
+        )',
+        worker.documents.active.distinct.pluck(:id)
+      )
+      .distinct
+      .pluck(:id)
+  end
+end
