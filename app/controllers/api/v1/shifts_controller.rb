961737c7f5fbfef3504e1854ca4b84f344a8a301
Thiago Henrique Leite da Silva <thiago.leite@redealumni.com>
2024-09-03 12:29:55 -0300

    refactor: update ShiftEligibilityService to use keyword arguments

--- app/controllers/api/v1/shifts_controller.rb
+++ app/controllers/api/v1/shifts_controller.rb
@@ -13,7 +13,7 @@ module Api
         cache_key = search_params.map { |key, value| "#{key}_#{value}" }.join('_')
 
         Rails.cache.fetch(cache_key, expires_in: 5.minutes) do
-          ShiftEligibilityService.new(search_params).eligible_shifts_for_worker
+          ShiftEligibilityService.new(**search_params).eligible_shifts_for_worker
         end
       end
 
