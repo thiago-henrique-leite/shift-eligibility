50d547edaa4ab4d701f195a121e83a6ba48d06a4
Thiago Henrique Leite da Silva <thiago.leite@redealumni.com>
2024-09-03 12:26:22 -0300

    refactor(controllers): add pagination method to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::API
+  def pagination(collection)
+    Kaminari.paginate_array(collection).page(page).per(per_page)
+  end
+
   def page
     params[:page] || 1
   end
