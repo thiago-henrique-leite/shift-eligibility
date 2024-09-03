29c48fe1fe8c054888b87bd35442646cdc574fa4
Thiago Henrique Leite da Silva <thiago.leite@redealumni.com>
2024-09-03 12:29:38 -0300

    feat(services): add validations and update params to ShiftEligibilityService

--- app/services/shift_eligibility_service.rb
+++ app/services/shift_eligibility_service.rb
@@ -1,25 +1,30 @@
 class ShiftEligibilityService
-  def initialize(worker_id, start_date, end_date)
+  def initialize(worker_id:, start_date:, end_date:, facility_id: nil)
     @worker_id = worker_id
     @start_date = start_date
     @end_date = end_date
-  end
-
-  def self.eligible_shifts_for_worker(*args)
-    new(*args).eligible_shifts_for_worker
+    @facility_id = facility_id
   end
 
   def eligible_shifts_for_worker
-    return [] unless worker.is_active
+    validate!
 
-    ShiftRepository.shifts_for_worker(worker, start_date, end_date)
+    return [] unless worker.is_active?
+
+    ShiftRepository.shifts_for_worker(worker, start_date, end_date, facility_id)
   end
 
   private
 
-  attr_reader :worker_id, :start_date, :end_date
+  attr_reader :worker_id, :start_date, :end_date, :facility_id
+
+  def validate!
+    raise ArgumentError, 'worker_id invalid' if worker.nil?
+    raise ArgumentError, 'start_date is required' if start_date.nil?
+    raise ArgumentError, 'end_date is required' if end_date.nil?
+  end
 
   def worker
-    @worker ||= Worker.find(worker_id)
+    @worker ||= Worker.find_by(id: worker_id)
   end
 end
