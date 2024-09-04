eee621e63a497573b211f1f2fa925060ca92911c
Thiago Henrique Leite da Silva <thiago.leite@redealumni.com>
2024-09-03 21:05:58 -0300

    refactor: Update ShiftsController to include facility_id in cache key

--- app/controllers/api/v1/shifts_controller.rb
+++ app/controllers/api/v1/shifts_controller.rb
@@ -1,24 +1,28 @@
 module Api
   module V1
     class ShiftsController < ApplicationController
+      include ExceptionHandler
+
       def index
         render json: pagination(available_shifts)
-      rescue ArgumentError => e
-        render json: { error: e.message }, status: :bad_request
       end
 
       private
 
       def available_shifts
-        cache_key = "shifts/#{worker_id}/#{start_date}/#{end_date}/#{page}/#{per_page}"
+        cache_key = "shifts/#{worker_id}/#{facility_id}/#{start_date}/#{end_date}/#{page}/#{per_page}"
 
         Rails.cache.fetch(cache_key, expires_in: 5.minutes) do
-          ShiftEligibilityService.new(worker_id, start_date, end_date).eligible_shifts_for_worker
+          ShiftEligibilityService.new(worker_id, facility_id, start_date, end_date).eligible_shifts_for_worker
         end
       end
 
       def end_date
         params[:end_date]
+      end
+
+      def facility_id
+        params[:facility_id]
       end
 
       def start_date
